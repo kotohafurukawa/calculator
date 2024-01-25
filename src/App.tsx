9e4c54479b8759a119eb7208830743ce0d83cb6d
kotohafurukawa <kotoha.furukawa00@gmail.com>
2024-01-25 21:37:40 +0900

    :add モック作成
    Buttonの機能ごとにコンポーネント毎に分ける

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,85 @@
+import { useState } from "react";
 import { Card, CardBody } from "@nextui-org/react";
+import { NumBtn } from "./components/NumBtn";
+import { PlusBtn } from "./components/PlusBtn";
+import { MinusBtn } from "./components/MinusBtn";
+import { MultipliedBtn } from "./components/MultipliedBtn";
+import { DividedBtn } from "./components/DividedBtn";
+import { DotBtn } from "./components/DotBtn";
+import { EqualBtn } from "./components/EqualBtn";
+import { ClearBtn } from "./components/ClearBtn";
 
 const App = () => {
+  const [state, setState] = useState(0);
   return (
-    <Card>
-      <CardBody>
-        <p>Make beautiful websites regardless of your design experience.</p>
-      </CardBody>
-    </Card>
+    <div className="max-w-sm mx-auto px-4 md:px-0">
+      <Card>
+        <CardBody>
+          <p>{state}</p>
+        </CardBody>
+      </Card>
+      <div className="pt-4 text-center">
+        <ul className="flex items-center justify-between">
+          <li>
+            <NumBtn number={7} />
+          </li>
+          <li>
+            <NumBtn number={8} />
+          </li>
+          <li>
+            <NumBtn number={9} />
+          </li>
+          <li>
+            <PlusBtn />
+          </li>
+        </ul>
+        <ul className="flex items-center justify-between pt-2">
+          <li>
+            <NumBtn number={4} />
+          </li>
+          <li>
+            <NumBtn number={5} />
+          </li>
+          <li>
+            <NumBtn number={6} />
+          </li>
+          <li>
+            <MinusBtn />
+          </li>
+        </ul>
+        <ul className="flex items-center justify-between pt-2">
+          <li>
+            <NumBtn number={1} />
+          </li>
+          <li>
+            <NumBtn number={2} />
+          </li>
+          <li>
+            <NumBtn number={3} />
+          </li>
+          <li>
+            <MultipliedBtn />
+          </li>
+        </ul>
+        <ul className="flex items-center justify-between pt-2">
+          <li>
+            <NumBtn number={0} />
+          </li>
+          <li>
+            <DividedBtn />
+          </li>
+          <li>
+            <DotBtn />
+          </li>
+          <li>
+            <EqualBtn />
+          </li>
+        </ul>
+        <div className="mt-2">
+          <ClearBtn />
+        </div>
+      </div>
+    </div>
   );
 };
 
