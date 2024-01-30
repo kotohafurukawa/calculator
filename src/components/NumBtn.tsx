5a369fd70d9ce61a3b64ef0c67b7bb3a8895f6a4
kotohafurukawa <kotoha.furukawa00@gmail.com>
2024-01-30 21:23:33 +0900

    add: 桁上がり機能追加

--- src/components/NumBtn.tsx
+++ src/components/NumBtn.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@nextui-org/react";
+import { useDispatch } from "react-redux";
+import { carryUp } from "../store/modules/calc";
 type NumPropsType = {
   number: number;
 };
 
 export const NumBtn = ({ number }: NumPropsType) => {
+  const dispatch = useDispatch();
   return (
     <Button
       onPress={() => {
-        console.log(number);
+        dispatch(carryUp(number));
       }}
     >
       {number}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <Provider store={store}>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </Provider>
   </React.StrictMode>
 );
